ba7247a783bd59a16495c25528cef0c1d82345ce
tysonlin <tysonlin@umich.edu>
2023-04-12 17:02:31 -0400

    adding median to data analysis

--- analysis.py
+++ analysis.py
@@ -40,16 +40,22 @@ incorrect_mean = sum(incorrect_gestures) / len(incorrect_gestures)
 correct_mean = sum(correct_gestures) / len(correct_gestures)
 rating_mean = sum(rating) / len(rating)
 
+# calculate medians
+missed_median = statistics.median(missed_gestures)
+incorrect_median = statistics.median(incorrect_gestures)
+correct_median = statistics.median(correct_gestures)
+rating_median = statistics.median(rating)
+
 # calculate std deviation
 missed_stdev = statistics.stdev(missed_gestures)
 incorrect_stdev = statistics.stdev(incorrect_gestures)
 correct_stdev = statistics.stdev(correct_gestures)
 rating_stdev = statistics.stdev(rating)
 
-print("RATING\nMean:%f\nStandard Deviation:%f\n" % (rating_mean, rating_stdev))
-print("CORRECT\nMean:%f\nStandard Deviation:%f\n" % (correct_mean, correct_stdev))
-print("MISSED\nMean:%f\nStandard Deviation:%f\n" % (missed_mean, missed_stdev))
-print("INCORRECT\nMean:%f\nStandard Deviation:%f\n" % (incorrect_mean, incorrect_stdev))
+print("RATING\nMedian:%f\nMean:%f\nStandard Deviation:%f\n" % (rating_median, rating_mean, rating_stdev))
+print("CORRECT\nMedian:%f\nMean:%f\nStandard Deviation:%f\n" % (correct_median, correct_mean, correct_stdev))
+print("MISSED\nMedian:%f\nMean:%f\nStandard Deviation:%f\n" % (missed_median, missed_mean, missed_stdev))
+print("INCORRECT\nMedian:%f\nMean:%f\nStandard Deviation:%f\n" % (incorrect_median, incorrect_mean, incorrect_stdev))
 
 
 
